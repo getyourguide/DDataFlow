8205712efef6fbf59e131a5eabb6beadca90f7a2
Jean Carlo Machado <jean.machado@getyourguide.com>
2022-06-29 18:57:49 +0200

    simplify code

--- examples/pipeline.py
+++ examples/pipeline.py
@@ -8,16 +8,12 @@ if 'ENABLE_OFFLINE_MODE' not in os.environ:
     spark.read.parquet("/tmp/demo_locations.parquet").registerTempTable("demo_locations")
     spark.read.parquet("/tmp/demo_tours.parquet").registerTempTable("demo_tours")
 
-def inspect_dataframes():
-    # pyspark code using a different source name
-    total_locations = spark.table(ddataflow.name('demo_locations')).count()
-    # sql code also works
-    total_tours = spark.sql(f""" SELECT COUNT(1) from {ddataflow.name('demo_tours')}""").collect()[0]['count(1)']
-    print({
-        "total_locations": total_locations,
-        "total_tours": total_tours,
-    })
-
-
-inspect_dataframes()
+# pyspark code using a different source name
+total_locations = spark.table(ddataflow.name('demo_locations')).count()
+# sql code also works
+total_tours = spark.sql(f""" SELECT COUNT(1) from {ddataflow.name('demo_tours')}""").collect()[0]['count(1)']
+print({
+    "total_locations": total_locations,
+    "total_tours": total_tours,
+})
 
